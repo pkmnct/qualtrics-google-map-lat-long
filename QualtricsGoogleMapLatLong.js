9953b00453fa26dc3580fd519fbc936d8e4cfbf6
George Walker <george@georgewwalker.com>
2017-03-06 10:00:08 -0600

    Bugfix: Google map events only fired once
    Created the function displayMap that replaces the script event
    listener, so that multiple map questions can exist on different pages
    of the survey. This also corrects the bug that if a response was
    required, the map would no longer display.

--- QualtricsGoogleMapLatLong.js
+++ QualtricsGoogleMapLatLong.js
@@ -3,7 +3,7 @@
  *
  * Written by George Walker <george@georgewwalker.com>
  *
- * Last changed March 2, 2017.
+ * Last changed March 6, 2017.
  *
  * This javascript allows a Qualtrics user to collect a lat/long from a
  * Google Map in a survey. To use it, create a new "Text Entry" question,
@@ -20,97 +20,97 @@ var googleMapAPIKey = "Your Key";
 // Load the Google Maps API if it is not already loaded.
 try {
     if (typeof googleMapJS == 'undefined') {
-    	var googleMapJS;
-    	if (googleMapJS == null) {
-    		googleMapJS = document.createElement('script');
-    		if (googleMapAPIKey == "Your Key" || googleMapAPIKey == null) {
-    			googleMapJS.src = 'https://maps.googleapis.com/maps/api/js';
-    		} else {
-    			googleMapJS.src = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapAPIKey;
-    		}
-    		document.head.appendChild(googleMapJS);
-    	}
+        var googleMapJS;
+        if (googleMapJS == null) {
+            googleMapJS = document.createElement('script');
+            if (googleMapAPIKey == "Your Key" || googleMapAPIKey == null) {
+                googleMapJS.src = 'https://maps.googleapis.com/maps/api/js';
+            } else {
+                googleMapJS.src = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapAPIKey;
+            }
+            document.head.appendChild(googleMapJS);
+        }
+    } else {
+        console.log("Map already loaded.");
     }
 } catch (err) {
     console.log("Unable to load Google Maps API. Details: " + err);
     alert("Unable to load Google Maps API.");
 }
 
-Qualtrics.SurveyEngine.addOnload(function()
-{
-	// Variables:
-	var mapCenterLat = 39.1836;
-	var mapCenterLng = -96.5717;
-	var mapZoom = 16; // See https://developers.google.com/maps/documentation/javascript/tutorial#zoom-levels for help.
-	var pinTitle = "Move pin to correct location"; // This is displayed when hovering over the pin on the map.
+Qualtrics.SurveyEngine.addOnload(function() {
+    // Variables:
+    var mapCenterLat = 39.1836;
+    var mapCenterLng = -96.5717;
+    var mapZoom = 16; // See https://developers.google.com/maps/documentation/javascript/tutorial#zoom-levels for help.
+    var pinTitle = "Move pin to correct location"; // This is displayed when hovering over the pin on the map.
 
 
-	var mapWidth = "100%";
-	var mapHeight = "300px";
+    var mapWidth = "100%";
+    var mapHeight = "300px";
 
 
-	// Get the data entry box and store it in a variable
-	var dataBox = document.getElementById("QR~" + this.questionId);
+    // Get the data entry box and store it in a variable
+    var dataBox = document.getElementById("QR~" + this.questionId);
 
-	// Get the question container and store it in a variable.
-	var questionContainer = this.getQuestionContainer();
+    // Get the question container and store it in a variable.
+    var questionContainer = this.getQuestionContainer();
 
     try {
         // Create a map object and append it to the question container.
-    	var mapObject = document.createElement('div');
-    	mapObject.setAttribute("id", this.questionId + "-map");
-    	mapObject.style.width = mapWidth;
-    	mapObject.style.height = mapHeight;
-    	questionContainer.appendChild(mapObject);
-    	var mapID = this.questionId + "-map";
+        var mapObject = document.createElement('div');
+        mapObject.setAttribute("id", this.questionId + "-map");
+        mapObject.style.width = mapWidth;
+        mapObject.style.height = mapHeight;
+        questionContainer.appendChild(mapObject);
+        var mapID = this.questionId + "-map";
     } catch (err) {
         console.log("Unable to create map object. Details: " + err);
         alert("An error occurred creating the map.");
     }
 
-	// Hide the data box
+    // Hide the data box
     try {
-	    dataBox.style.display = 'none';
+        dataBox.style.display = 'none';
     } catch (err) {
         console.log("Unable to hide data box.");
     }
 
-	// Add event listener to perform Google Map stuff once API is loaded.
-    try {
-        googleMapJS.addEventListener('load', function() {
+    // This function calls itself once per second until the Google Maps API is loaded, then it displays the map.
+    function displayMap() {
+        try {
+            var map = new google.maps.Map(document.getElementById(mapID), {
+                center: {
+                    lat: mapCenterLat,
+                    lng: mapCenterLng
+                },
+                zoom: mapZoom
+            });
 
-    		// Store the Google Map in a variable
-    		var map = new google.maps.Map(document.getElementById(mapID), {
-    			center: {
-    				lat: mapCenterLat,
-    				lng: mapCenterLng
-    			},
-    			zoom: mapZoom
-    		});
+            // Create a new marker in the center of the map.
+            var marker = new google.maps.Marker({
+                draggable: true,
+                position: {
+                    lat: mapCenterLat,
+                    lng: mapCenterLng
+                },
+                map: map,
+                title: pinTitle
+            });
 
-    		// Create a new marker in the center of the map.
-    		var marker = new google.maps.Marker({
-    			draggable: true,
-    			position: {
-    				lat: mapCenterLat,
-    				lng: mapCenterLng
-    			},
-    			map: map,
-    			title: pinTitle
-    		});
+            // When the pin is clicked, store the lat/lng
+            google.maps.event.addListener(marker, 'click', function(event) {
+                dataBox.value = '{"lat": "' + this.getPosition().lat() + '", "long": "' + this.getPosition().lng() + '"}';
+            });
 
-    		// When the pin is clicked, store the lat/lng
-    		google.maps.event.addListener(marker, 'click', function(event) {
-    			dataBox.value = '{"lat": "' + this.getPosition().lat() + '", "long": "' + this.getPosition().lng() + '"}';
-    		});
-
-    		// When the pin is dragged, store the lat/lng where it ends
-    		google.maps.event.addListener(marker, 'dragend', function(event) {
-    			dataBox.value = '{"lat": "' + this.getPosition().lat() + '", "long": "' + this.getPosition().lng() + '"}';
-    		});
-    	});
-    } catch (err) {
-        console.log("Unable to create event listener. Details: " + err);
-        alert("An error occurred with this question. Please contact the survey creator.");
+            // When the pin is dragged, store the lat/lng where it ends
+            google.maps.event.addListener(marker, 'dragend', function(event) {
+                dataBox.value = '{"lat": "' + this.getPosition().lat() + '", "long": "' + this.getPosition().lng() + '"}';
+            });
+        } catch (err) {
+            setTimeout(displayMap, 1000);
+        }
     }
+    displayMap();
+
 });
